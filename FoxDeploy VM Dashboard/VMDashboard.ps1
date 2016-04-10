d405ec995adac5c2ce24addbbc81eac60e8c9f85
Stephen Owen <sred13@gmail.com>
2016-04-10 00:56:05 -0400

    forgot to upload final version

--- FoxDeploy VM Dashboard/VMDashboard.ps1
+++ FoxDeploy VM Dashboard/VMDashboard.ps1
@@ -1,44 +1,54 @@
-﻿$main = $Null 
-$VMS = get-vm | sort State 
-$head = ((gc .\head.html) -replace '%4',(get-date).DateTime) -replace '%t'.("VM Report")
-$tail = gc .\tail.html
+#zero out variables before beginning (helps for working on file in ISE
+$main = $Null 
 $i = 0 
 
+$VMS = Get-VM | sort State 
+
+$head = (Get-Content .\head.html) -replace '%4',(get-date).DateTime
+$tail = Get-Content .\tail.html
+
+
 ForEach ($VM in $VMS){
-$i++
-        $description= @"
+    
+    #increment $i, to use the many different background image options
+    $i++
+    
+    #set the name 
+    $Name=$vm.Name
+    
+    #choose which style (which affect the color of the card generated)
+    if ($vm.State -eq 'Off'){
+        $style = 'style1'
+        }
+        elseif($vm.state -eq 'Running'){
+        $style = "style2"
+        }
+        else{
+        #VM is haunted or something else
+        $style = "style3"
+        }
+    
+    #if the VM is on, don't just show it's name, but it's RAM and CPU usage too
+    if ($VM.State -eq 'Running'){
+     
+        $Name="$($VM.Name)<br>
+         RAM: $($VM.MemoryAssigned /1mb)<br>
+         CPU: $($VM.CPUUsage)"
+    }
+
+    #Set the flavor text, which appears when we hover over the card
+    $description= @"
         Currently $($VM.Status.ToLower()) with a <br>
         state of $($VM.State)<br>
         It was created on $($VM.CreationTime)<br>
-
         Its files are found in $($VM.Path)
-
-
 "@
 
-if ($vm.State -eq 'Off'){
-    $image = "images/pic01.jpg";$style = 'style1'
-    $Name=$vm.Name
-    }else{
-    "VM isnt off"
-    $image = "images/pic03.jpg";$style = "style3"
-    $Name="$($VM.Name)<br>
-    RAM: $($VM.MemoryAssigned /1mb)<br>
-    CPU: $($VM.CPUUsage)
-    "
-    }
-
-    $image = "images/pic0$i.jpg"
-
-
-        $style = "<article class=`"$($style)`">"
-        "setting style as : $style"
-
-        $tile = @"
-                                
-                                $style
+    #make a card
+    $tile = @"
+                                <article class="$style">
 									<span class="image">
-										<img src="$image" alt="" />
+										<img src="images/pic01.jpg" alt="" />
 									</span>
 									<a href="generic.html">
 										<h2>$Name</h2>
@@ -48,7 +58,7 @@ if ($vm.State -eq 'Off'){
 									</a>
 								</article>
 "@
-$tile
+
 
 $main += $tile
 }
